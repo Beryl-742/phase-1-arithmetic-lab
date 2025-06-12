19e5cd4f29f8dc2e5a781b7adf858dacc678a327
Beryl-742 <Berylawuor742@gmail.com>
2025-06-12 22:04:29 +0300

    Arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 16;
+const num4 = 6;
+const multiply = num1 * num2; // 2 * 31 =62
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const mod = num3 % num4; // 16 % 6 = 4
+
+const max = Math.max(5, 10, 15, 20); //=> 20
+
+
